cdf1f1c000615b161aa853c8f4278db47861c40c
mdinos <32902031+mdinos@users.noreply.github.com>
2019-08-24 01:32:44 +0100

    Initial working html template

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# golang
+# simple-go-webserver
+
+trying out go
+
+Uses the net/http package to server templated html pages
--- main.go
+++ main.go
@@ -1,6 +1,51 @@
 package main
 
+import (
+    "net/http"
+    "crypto/rand"
+    "math/big"
+    "time"
+    "fmt"
+    "html/template"
+)
+
 func main() {
-    println("hello!")
+    serveSlash()
 }
 
+func serveSlash() {
+    http.HandleFunc("/", hello)
+    http.HandleFunc("/index.html", hello)
+    http.ListenAndServe(":9021", nil)
+}
+
+type tempContents struct {
+    RandNum string
+    Name    string
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    start := time.Now()
+    bigInt := big.NewInt(int64(5000))
+    num, err := rand.Int(rand.Reader, bigInt)
+    if err != nil {
+        fmt.Println(err)
+    }
+    numStr := num.String()
+    respString := r.FormValue("name")
+    if respString == "" {
+        respString = "Nameless"
+    }
+    contents := tempContents{
+        RandNum: numStr, 
+        Name: respString,
+    }
+    tmpl, err := template.ParseFiles("html/index.html")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("[200] OK " + respString + ", " + numStr)
+    duration := time.Since(start) * 100
+    fmt.Println(duration)
+    tmpl.Execute(w, contents)
+}
