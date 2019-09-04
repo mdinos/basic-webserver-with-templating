77ba3b8e7cf6330406853a7da474720e98ac8502
mdinos <32902031+mdinos@users.noreply.github.com>
2019-09-04 14:55:11 +0100

    idk

--- main.go
+++ main.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-    "net/http"
-    "crypto/rand"
-    "math/big"
-    "time"
-    "fmt"
-    "html/template"
+	"crypto/rand"
+	"fmt"
+	"html/template"
+	"math/big"
+	"net/http"
+	"time"
 )
 
 type tempContents struct {
-    RandNum string
-    Name    string
+	RandNum string
+	Name    string
 }
 
 func main() {
-    serve("9021")
+	serve("9021")
 }
 
 func serve(port string) {
-    http.HandleFunc("/", index)
-    http.HandleFunc("/index.html", index)
-    http.ListenAndServe(":" + port, nil)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/index.html", index)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func randInt(m int) (r string) {
-    bigInt := big.NewInt(int64(m))
-    num, err := rand.Int(rand.Reader, bigInt)
-    if err != nil {
-        fmt.Println(err)
-    }
-    numStr := num.String()
-    return numStr
+	bigInt := big.NewInt(int64(m))
+	num, err := rand.Int(rand.Reader, bigInt)
+	if err != nil {
+		fmt.Println(err)
+	}
+	numStr := num.String()
+	return numStr
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-    start := time.Now()
-    randomInteger := randInt(5000)
-    name := r.FormValue("name")
-    if name == "" {
-        name = "Nameless"
-    }
-    contents := tempContents{
-        RandNum: randomInteger,
-        Name: name,
-    }
-    tmpl, err := template.ParseFiles("html/index.html")
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("[200] OK " + name + ", " + randomInteger)
-    duration := time.Since(start) * 100
-    fmt.Println(duration)
-    tmpl.Execute(w, contents)
+	start := time.Now()
+	randomInteger := randInt(5000)
+	name := r.FormValue("name")
+	if name == "" {
+		name = "Nameless"
+	}
+	contents := tempContents{
+		RandNum: randomInteger,
+		Name:    name,
+	}
+	tmpl, err := template.ParseFiles("html/index.html")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("[200] OK " + name + ", " + randomInteger)
+	duration := time.Since(start) * 100
+	fmt.Println(duration)
+	tmpl.Execute(w, contents)
 }
